7f640d3f44385990f57795abebd22cfb17974f79
vasukanster <vasukanster@gmail.com>
2024-10-02 21:33:59 +0530

    Create Kruskal (MST): Really Special Subtree.cpp

---
+++ Kruskal (MST): Really Special Subtree.cpp
@@ -0,0 +1,182 @@
+/*Kruskal (MST): Really Special Subtree
+link: https://www.hackerrank.com/challenges/kruskalmstrsub/problem
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'kruskals' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts WEIGHTED_INTEGER_GRAPH g as parameter.
+ */
+
+/*
+ * For the weighted graph, <name>:
+ *
+ * 1. The number of nodes is <name>_nodes.
+ * 2. The number of edges is <name>_edges.
+ * 3. An edge exists between <name>_from[i] and <name>_to[i]. The weight of the edge is <name>_weight[i].
+ *
+ */
+ 
+ class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+// Kruskal's algorithm to find the minimum spanning tree (MST)
+int kruskals(int g_nodes, vector<int> g_from, vector<int> g_to, vector<int> g_weight) {
+    vector<pair<int, pair<int, int>>> edges; // Vector to store edges in the form (weight, (node1, node2))
+    for (int i = 0; i < g_from.size(); i++) {
+        int adjNode = g_to[i]; // Get the adjacent node
+        int wt = g_weight[i]; // Get the weight of the edge
+        int node = g_from[i]; // Get the source node
+        edges.push_back({wt, {node, adjNode}}); // Add edge to the list
+    }
+
+    DisjointSet ds(g_nodes); // Initialize the disjoint set
+    sort(edges.begin(), edges.end()); // Sort edges by weight
+    int mstWt = 0; // Initialize the total weight of the MST
+    for (auto it : edges) { // Iterate over sorted edges
+        int wt = it.first; // Get the weight of the edge
+        int u = it.second.first; // Get the first node of the edge
+        int v = it.second.second; // Get the second node of the edge
+
+        // If u and v belong to different sets, include this edge in the MST
+        if (ds.findUPar(u) != ds.findUPar(v)) {
+            mstWt += wt; // Add weight to the total MST weight
+            ds.unionBySize(u, v); // Union the two sets
+        }
+    }
+    return mstWt; // Return the total weight of the MST
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string g_nodes_edges_temp;
+    getline(cin, g_nodes_edges_temp);
+
+    vector<string> g_nodes_edges = split(rtrim(g_nodes_edges_temp));
+
+    int g_nodes = stoi(g_nodes_edges[0]);
+    int g_edges = stoi(g_nodes_edges[1]);
+
+    vector<int> g_from(g_edges);
+    vector<int> g_to(g_edges);
+    vector<int> g_weight(g_edges);
+
+    for (int i = 0; i < g_edges; i++) {
+        string g_from_to_weight_temp;
+        getline(cin, g_from_to_weight_temp);
+
+        vector<string> g_from_to_weight = split(rtrim(g_from_to_weight_temp));
+
+        int g_from_temp = stoi(g_from_to_weight[0]);
+        int g_to_temp = stoi(g_from_to_weight[1]);
+        int g_weight_temp = stoi(g_from_to_weight[2]);
+
+        g_from[i] = g_from_temp;
+        g_to[i] = g_to_temp;
+        g_weight[i] = g_weight_temp;
+    }
+
+    int res = kruskals(g_nodes, g_from, g_to, g_weight);
+
+    // Write your code here.
+    fout<<res<<endl;
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
